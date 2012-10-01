ef7ef73221b99aa93e35331dac7dfa97e7cb549e
Henrik <henrik@haf.se>
2012-10-01 14:48:27 +0200

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: puppet-httpfile
+#
+# This module manages puppet-httpfile
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class puppet-httpfile {
+
+
+}
